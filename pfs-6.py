515c831ae1fcb882655105e04c6bb2056f31d7bb
adeebadel <adelarective@gmail.com>
2026-06-28 01:16:16 +0530

    pfs-6

---
+++ pfs-6.py
@@ -0,0 +1,13 @@
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+percentage = float(input("Enter your percentage in % : "))
+
+print(name)
+print(age)
+print(percentage,"%")
+
+height = float(input("Enter your height in ft : "))
+weight = float(input("Enter your weight in kg : "))
+
+print(height,"ft")
+print(weight,"kg")
