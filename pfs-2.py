4b013699c058b7738a352ec1b241c31bdc950202
adeebadel <adelarective@gmail.com>
2026-06-27 20:41:03 +0530

    pfs-2

---
+++ pfs-2.py
@@ -0,0 +1,21 @@
+name = "Adeeb"
+age = 20
+city = "bengaluru"
+
+coin = 10
+print(coin)
+coin = 50
+print(coin)
+
+print(name)
+print(age)
+print(city)
+
+name = "Adeeb"
+city = "shanivarasanthe"
+age = 20 
+print(name,age,city)
+
+name = "Adeeb"
+dream = "goat"
+print("name : ", name)
