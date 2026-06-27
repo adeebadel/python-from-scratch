3464f513d29690c1f87f48ec90e8c6b73b7264b0
adeebadel <adelarective@gmail.com>
2026-06-28 02:19:25 +0530

    pfs-10

---
+++ pfs-10.py
@@ -0,0 +1,16 @@
+age = 20
+if age>=21:
+    print("you can drink")
+else:
+    print("you can't drink")
+    
+marks = 95
+
+if marks>= 90:
+    print("grade A")
+elif marks>=80:
+    print("grade B")
+elif marks>=70:
+    print("grade c")
+else:
+    print("grade d")
