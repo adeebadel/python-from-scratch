5c911cc91f3ebd647e48ee10c0100ce69d86085f
adeebadel <adelarective@gmail.com>
2026-06-28 01:40:05 +0530

    pfs-8

---
+++ pfs-8.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+print("sum=",num1+num2)
+print("subtract=",num1-num2)
+print("divide=",num1/num2)
+print("multiply=",num1*num2)
+
+num3 = float(input("Enter the number : "))
+num4 = float(input("Enter the divisor : "))
+
+print("Remainder = ", num3%num4)
