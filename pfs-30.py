45cfd5b217e621a14d992e5cd5774208124305ad
adeebadel <adelarective@gmail.com>
2026-06-29 01:44:24 +0530

    pfs-30

---
+++ pfs-30.py
@@ -0,0 +1,13 @@
+fruits = ["Apple","Orange","Banana"]
+fruits.insert(1,"Mango")
+print(fruits)
+fruits.pop(1)
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+
+numbers = [ 1,2,3,4,5,6,7,8,9,9,9,9,9,9,9,9,9,9]
+print(numbers.count(9))
+print(numbers.index(9))
