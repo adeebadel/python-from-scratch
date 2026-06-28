06ad05ae227473690325017f6bb306d797321e58
adeebadel <adelarective@gmail.com>
2026-06-28 16:57:13 +0530

    pfs-15

---
+++ pfs-15.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    print("train")
+    
+for i in range(6):
+    print(i)
+
+for i in range(1,7):
+    print(i)
+print("Hello world")
+
+for i in range(2,11,2):
+    print(i)
+    
+print("SIUUU")
+
+for i in range(10,0,-2):
+    print(i)
