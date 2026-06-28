624ec5b37f888e972721acd730b9ccee91ce4190
adeebadel <adelarective@gmail.com>
2026-06-28 18:47:14 +0530

    pfs-18

---
+++ pfs-18.py
@@ -0,0 +1,11 @@
+for i in range(1,6):
+    print("*"*i)
+print("hello")  
+for i in range(7,0,-1):
+    print("*"*i)
+    
+for i in range(1,-2,6):
+    print("*"*i)
+
+for i in range(1, 6):
+    print(" " * (5 - i) + "*" * (2 * i - 1))
