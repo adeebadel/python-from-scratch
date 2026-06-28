18823b7551388b7b57e5f9cbdac9b2953f878a21
adeebadel <adelarective@gmail.com>
2026-06-28 20:51:09 +0530

    pfs-28

---
+++ pfs-28.py
@@ -0,0 +1,15 @@
+movies = ["End game","Infinity war","Doomsday","Age of ultron"]
+print(movies)
+cities = ["Bengaluru","Hyderabad","Delhi","Mumbai"]
+print(cities[0],cities[3])
+sports = ["Football","MMA","Cricket","Kabaddi"]
+print(sports)
+sports.append("Athletics")
+print(sports)
+subjects = ["math","science","geography","english","history"]
+print(subjects)
+subjects.remove("science")
+print(subjects)
+subjects[2]="Programming"
+print(subjects)
+print(len(subjects))
