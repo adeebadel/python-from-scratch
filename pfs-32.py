9e1776314f6e699f56cb91e95f864e919a94703c
adeebadel <adelarective@gmail.com>
2026-06-29 01:59:41 +0530

    pfs-32

---
+++ pfs-32.py
@@ -0,0 +1,26 @@
+def greet():
+    print("hello")
+greet()
+
+def greet(name):
+    print("hello",name)
+greet("adeeb")
+
+def greet(name):
+    print("hello",name)
+greet("Adeeb")
+greet("Ronaldo")
+
+def student(name,age):
+    print(name)
+    print(age)
+student("Adeeb",20)
+
+def add(a,b):
+    return a + b
+result = add(10,5)
+print(result)
+
+def square(num):
+    return num * num
+print(square(4))
