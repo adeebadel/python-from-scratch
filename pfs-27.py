05adf471e881790905b8a079209c70d3e9b90735
adeebadel <adelarective@gmail.com>
2026-06-28 20:41:53 +0530

    pfs-27

---
+++ pfs-27.py
@@ -0,0 +1,12 @@
+fruits = ["Apple","Banana","Mango","Blueberry","Grape"]
+print(fruits)
+print(fruits[1])
+fruits[1]="Rasberry"
+print(fruits)
+fruits.append("Guava")
+print(fruits)
+fruits.remove("Blueberry")
+print(fruits)
+print(len(fruits))
+for fruit in fruits:
+    print(fruit)
