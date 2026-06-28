712923469b47cded9def9599f1a1247ef3463929
adeebadel <adelarective@gmail.com>
2026-06-28 16:52:25 +0530

    pfs-14

---
+++ pfs-14.py
@@ -0,0 +1,8 @@
+username = "Adeeb123"
+password = "neymar"
+enter_username = input("Enter username : ")
+enter_password = input("Enter password : ")
+if enter_username == username and enter_password == password:
+    print("Access granted")
+else:
+    print("wrong username or password")
