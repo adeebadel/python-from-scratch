1ae6f7629a00c5bd99b9208477dbc8f0251545aa
adeebadel <adelarective@gmail.com>
2026-06-28 16:29:52 +0530

    pfs-12

---
+++ pfs-12.py
@@ -0,0 +1,22 @@
+num = int(input("Enter your number : "))
+if num >=1:
+    print("number is positive")
+elif num == 0:
+    print("number is zero")
+else:
+    print("number is negative")
+    
+marks = int(input("Enter a number : "))
+if marks >=35 :
+    print("pass")
+else:
+    print("fail")
+    
+num1 = int(input("Enter a number : "))
+num2 = int(input("Enter a number : "))
+if num1>num2:
+    print("num1 is greater")
+elif num1==num2:
+    print("both are equal")
+else:
+    print("num2 is greater")
