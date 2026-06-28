91a1e8d7babb92c5512afd51ffec00480af52641
adeebadel <adelarective@gmail.com>
2026-06-28 16:21:37 +0530

    pfs-11

---
+++ pfs-11.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age : "))
+if age>= 18:
+    print("you can vote")
+else:
+    print("you can't vote")
+    
+num = int(input("Enter the num : "))
+if num % 2 == 0:
+    print("num is even")
+else:
+    print("num is odd")
+# a - ( b * (a//b))
+
