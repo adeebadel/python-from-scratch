1b549bec5b3ae4a818775e738f26bc470dcb93b1
adeebadel <adelarective@gmail.com>
2026-06-29 01:51:06 +0530

    pfs-31

---
+++ pfs-31.py
@@ -0,0 +1,17 @@
+languages = ["Python", "Java", "C++", "JavaScript"]
+languages.reverse()
+print(languages)
+
+marks = [90, 80, 90, 70, 90, 60]
+print(marks.count(90))
+
+cities = ["Delhi", "Mumbai", "Bangalore", "Chennai"]
+print(cities.index("Bangalore"))
+
+players = ["Messi", "Ronaldo", "Neymar"]
+players.insert(0,"Mbappe")
+print(players)
+players.sort()
+print(players)
+players.reverse()
+print(players)
