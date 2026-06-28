a3795f017d47160fa06ac055cec9b1de9903d596
adeebadel <adelarective@gmail.com>
2026-06-28 19:11:39 +0530

    pfs-21

---
+++ pfs-21.py
@@ -0,0 +1,15 @@
+count = 1
+while count <= 10:
+    print(count)
+    count = count + 1
+    
+banana = 2
+while banana <= 20:
+    print(banana)
+    banana = banana +2
+
+chocolate = 10
+while chocolate >=1:
+    print(chocolate)
+    chocolate = chocolate - 1
+print("Lift off")
