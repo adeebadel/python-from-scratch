b3a7cd9e11e6c0e4287209618370d94cbb93a8e5
adeebadel <adelarective@gmail.com>
2026-06-28 19:06:58 +0530

    pfs-20

---
+++ pfs-20.py
@@ -0,0 +1,17 @@
+banana = 5
+
+while banana>=1:
+    print(banana)
+    banana = banana - 1
+    
+num = int(input("enter a number : "))
+car = 1
+while car <= num:
+    print(car)
+    car = car + 1
+    
+num2 = int(input("enter another number : "))
+bike = 5
+while bike>=num2:
+    print(bike)
+    bike = bike - 1
