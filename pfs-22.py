240b7d977fe3fc7475b86d1a6f81da6c18a8ebf6
adeebadel <adelarective@gmail.com>
2026-06-28 19:42:35 +0530

    pfs-22

---
+++ pfs-22.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number : "))
+count = 1
+while count <= 10:
+    print(num,"x",count,"=",num*count)
+    count = count + 1
+    
+num2 = int(input("Enter a number : "))
+banana = 1
+total = 0
+while banana<=num2:
+    total = total + banana
+    banana = banana + 1
+    print(total)
