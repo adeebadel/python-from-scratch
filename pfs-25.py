618e8193e59f55745d29ec9294f4ee4d3a0c815e
adeebadel <adelarective@gmail.com>
2026-06-28 20:24:52 +0530

    pfs-25

---
+++ pfs-25.py
@@ -0,0 +1,14 @@
+for i in range(3):
+    for j in range(1,4):
+        print(j,end="")
+    print()
+    
+for i  in range(4):
+    for j in range(4):
+        print("*",end="")
+    print()
+    
+for i in range(3):
+    for j in range(1,6):
+        print(j,end="")
+    print()
